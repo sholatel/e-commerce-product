9ca63e9330c6cdad21a79c3c082b01a33bee2c8c
sholatel <yusuforiade1616@gmail.com>
2022-03-24 11:54:53 +0100

    Updates

---
+++ precache-manifest.eaee6fdcd1d32d6f79d149d426fce6fe.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbca91726f45094c1011",
+    "url": "/e-commerce-product/static/css/main.fac7d482.chunk.css"
+  },
+  {
+    "revision": "dbca91726f45094c1011",
+    "url": "/e-commerce-product/static/js/main.dbca9172.chunk.js"
+  },
+  {
+    "revision": "08611444a190981da534",
+    "url": "/e-commerce-product/static/js/1.08611444.chunk.js"
+  },
+  {
+    "revision": "aa57b34f539c96d1b678",
+    "url": "/e-commerce-product/static/js/2.aa57b34f.chunk.js"
+  },
+  {
+    "revision": "cfb8b7d2c12051136153",
+    "url": "/e-commerce-product/static/js/runtime~main.cfb8b7d2.js"
+  },
+  {
+    "revision": "40b33a68c030acf274b0fa628a92fafe",
+    "url": "/e-commerce-product/static/media/icon-cart.40b33a68.svg"
+  },
+  {
+    "revision": "b0321cb3e6bf6aef8ecc31e32a577687",
+    "url": "/e-commerce-product/static/media/icon-delete.b0321cb3.svg"
+  },
+  {
+    "revision": "06305c96f6e5bbef5501440e19da4d62",
+    "url": "/e-commerce-product/static/media/logo.06305c96.svg"
+  },
+  {
+    "revision": "0ac65e8df3cdb3240e82aac50d5b84c5",
+    "url": "/e-commerce-product/static/media/image-product-1.0ac65e8d.jpg"
+  },
+  {
+    "revision": "dc5f56d918a4a172b00d9fb5c782f873",
+    "url": "/e-commerce-product/static/media/image-product-2.dc5f56d9.jpg"
+  },
+  {
+    "revision": "d8f0fd258d7833d69417c1cc12d217bb",
+    "url": "/e-commerce-product/static/media/image-product-3.d8f0fd25.jpg"
+  },
+  {
+    "revision": "5e95fcb499ff1a1c449b25f41fc5bf1b",
+    "url": "/e-commerce-product/static/media/image-product-4.5e95fcb4.jpg"
+  },
+  {
+    "revision": "2166e08d2f67f82606d57886e65f367c",
+    "url": "/e-commerce-product/static/media/icon-menu.2166e08d.svg"
+  },
+  {
+    "revision": "383d1284c6fd2728846d4d5033882509",
+    "url": "/e-commerce-product/static/media/icon-close.383d1284.svg"
+  },
+  {
+    "revision": "b02d406c9da0e59c42120e997154fbbb",
+    "url": "/e-commerce-product/static/media/icon-next.b02d406c.svg"
+  },
+  {
+    "revision": "11c68ec66c6da9819bfefe602bbb9e3b",
+    "url": "/e-commerce-product/static/media/icon-previous.11c68ec6.svg"
+  },
+  {
+    "revision": "0035f16e8254ffaef52d8039f515581a",
+    "url": "/e-commerce-product/static/media/icon-minus.0035f16e.svg"
+  },
+  {
+    "revision": "d13f859f0bc3a337e36dd0c85251c30c",
+    "url": "/e-commerce-product/static/media/icon-plus.d13f859f.svg"
+  },
+  {
+    "revision": "781475979eed5d04a8c62515f6bed415",
+    "url": "/e-commerce-product/static/media/icon-cart-copy.78147597.svg"
+  },
+  {
+    "revision": "6a289d11fd89757e0bbe6df22f10c9e9",
+    "url": "/e-commerce-product/static/media/KumbhSansFont.6a289d11.ttf"
+  },
+  {
+    "revision": "cc8bbf9a5d0e4e6709b7a5546da55074",
+    "url": "/e-commerce-product/index.html"
+  }
+];
